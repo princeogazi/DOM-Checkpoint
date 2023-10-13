0ae40a6bb3c6d0972d757d4e1d1b955742a4c834
princeogazi <142050651+princeogazi@users.noreply.github.com>
2023-10-13 19:28:49 +0100

    Second Commit

--- script.js
+++ script.js
@@ -45,3 +45,16 @@ cartItems.forEach((item) => {
     })
 
 })
+
+const grayHeart = document.querySelector('.gray-heart')
+const redHeart = document.querySelector('.red-heart')
+
+grayHeart.addEventListener("click", () => {
+    redHeart.classList.add("animation");
+    grayHeart.classList.add("fill-color");
+});
+
+redHeart.addEventListener("click", () => {
+    redHeart.classList.remove("animation");
+    grayHeart.classList.remove("fill-color");
+});
